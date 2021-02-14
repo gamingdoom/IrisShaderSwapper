577be8a126919a4b50a9e12b2aa8317dbb72bea8
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-02-14 12:53:56 -0800

    intial commit

---
+++ IrisShaderSwapper/IrisShaderSwapper/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace IrisShaderSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "Environment.SpecialFolder.ApplicationData";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose .minecraft folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose shader";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(169, 97);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(448, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Iris Shader Swapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ IrisShaderSwapper/IrisShaderSwapper/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IrisShaderSwapper
+{
+    public partial class Form1 : Form
+    {
+        public string dotMinecraft = "";
+        public string configLocation = "";
+        public string shaderLocation = "";
+        public string shaderPack = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft";
+            Console.WriteLine(appdata);
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.SelectedPath = appdata;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+                dotMinecraft = folderDlg.SelectedPath;
+                Console.WriteLine(dotMinecraft);
+                getConfig(dotMinecraft);
+                getShaders(dotMinecraft);
+            }
+        }
+        private void getConfig(string folder)
+        {
+            Console.WriteLine(folder);
+            configLocation = folder + @"\config\iris.properties";
+            Console.WriteLine(configLocation);
+        }
+        private void getShaders(string folder)
+        {
+            shaderLocation = folder + @"\shaderpacks";
+            //FIND ALL FILES IN FOLDER
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(shaderLocation);
+            foreach (System.IO.FileInfo f in dir.GetFiles("*.*"))
+            {
+                comboBox1.Items.Add(f.Name);
+            }
+        }
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            shaderPack = comboBox1.Text.ToString();
+            Console.WriteLine(shaderPack);
+            editConfig(shaderPack, configLocation);
+        }
+        private void editConfig(string shader, string config) 
+        {
+            try
+            {
+                // Check if file already exists. If yes, delete it.     
+                if (File.Exists(config))
+                {
+                    File.Delete(config);
+                }
+
+                // Create a new file     
+                using (StreamWriter sw = File.CreateText(config))
+                {
+                    sw.WriteLine(@"#This file stores configuration options for Iris, such as the currently active shaderpack");
+                    sw.WriteLine(@"#" + DateTime.Now.ToString());
+                    sw.WriteLine(@"shaderPack=" + shader);
+
+                }
+
+                // Write file contents on console.     
+                using (StreamReader sr = File.OpenText(config))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
