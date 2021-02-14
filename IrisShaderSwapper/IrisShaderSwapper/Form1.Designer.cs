25b4a97ada884ca43ebaee3304bcb9f5c72987ef
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-02-14 13:20:50 -0800

    When window is resized controls are resized

--- IrisShaderSwapper/IrisShaderSwapper/Form1.Designer.cs
+++ IrisShaderSwapper/IrisShaderSwapper/Form1.Designer.cs
@@ -44,6 +44,8 @@ namespace IrisShaderSwapper
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(308, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 23);
@@ -54,6 +56,9 @@ namespace IrisShaderSwapper
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(169, 41);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(448, 20);
@@ -61,6 +66,8 @@ namespace IrisShaderSwapper
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Location = new System.Drawing.Point(308, 68);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(159, 23);
@@ -71,6 +78,9 @@ namespace IrisShaderSwapper
             // 
             // comboBox1
             // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(169, 97);
             this.comboBox1.Name = "comboBox1";
